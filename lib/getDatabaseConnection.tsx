79d7f8414b6b338bb2faf273357c12c955198a4c
frankfang <frankfang1990@gmail.com>
2020-06-09 14:10:04 +0800

    展示 posts

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -1,15 +1,28 @@
 import {createConnection, getConnectionManager} from 'typeorm';
+import 'reflect-metadata';
+import {Post} from 'src/entity/Post';
+import {User} from 'src/entity/User';
+import {Comment} from 'src/entity/Comment';
+import config from 'ormconfig.json';
+
+const create = async () => {
+  // @ts-ignore
+  return createConnection({
+    ...config,
+    entities: [Post, User, Comment]
+  });
+};
 
 const promise = (async function () {
   const manager = getConnectionManager();
   if (!manager.has('default')) {
-    return createConnection();
+    return create();
   } else {
     const current = manager.get('default');
-    if(current.isConnected){
-      return current
-    }else{
-      return createConnection();
+    if (current.isConnected) {
+      return current;
+    } else {
+      return create();
     }
   }
 })();
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,38 +2,30 @@ import {GetServerSideProps, NextPage} from 'next';
 import {UAParser} from 'ua-parser-js';
 import {useEffect, useState} from 'react';
 import {getDatabaseConnection} from '../lib/getDatabaseConnection';
+import {Post} from '../src/entity/Post';
 
 type Props = {
-  browser: {
-    name: string;
-    version: string;
-    major: string;
-  }
+  posts: Post[]
 }
 const index: NextPage<Props> = (props) => {
-  const {browser} = props;
-  const [width, setWidth] = useState(0);
-  useEffect(() => {
-    const w = document.documentElement.clientWidth;
-    setWidth(w);
-  }, []);
+  const {posts} = props;
   return (
     <div>
-      <h1>你的浏览器是 {browser.name}</h1>
-    <h2>你的浏览器窗口大小是 {width} 像素</h2>
+      {posts.map(post => <div>{post.title}</div>)}
     </div>
   );
 };
 export default index;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const connect = await getDatabaseConnection()// 第一次链接能不能用 get
-  console.log('connect');
+  const connection = await getDatabaseConnection();// 第一次链接能不能用 get
+  const posts = await connection.manager.find(Post);
   const ua = context.req.headers['user-agent'];
   const result = new UAParser(ua).getResult();
   return {
     props: {
-      browser: result.browser
+      browser: result.browser,
+      posts: JSON.parse(JSON.stringify(posts))
     }
   };
 };
